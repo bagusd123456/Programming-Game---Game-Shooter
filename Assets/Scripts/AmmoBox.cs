4cdbf09922627ad6c1e6bd58dff1f7c7604adc7f
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2021-10-22 15:40:18 +0700

    Game Shooter Part 2 PlayerShoot, Enemy Controller

---
+++ Assets/Scripts/AmmoBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            WeaponController.ammoMag = 90;
+        }
+    }
+}
