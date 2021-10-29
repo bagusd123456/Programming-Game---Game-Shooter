ea34bf8fa246ea1c8d07a643af8da1351b00ccc0
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2021-10-29 15:25:52 +0700

    Part 3, Damage, Sound, Main Menu

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -20,6 +20,10 @@ public class WeaponController : MonoBehaviour
     public Slider healthBar;
     [SerializeField] Camera FPSCamera, TPSCamera;
 
+    public AudioSource AK47Shot;
+    public AudioSource ShotgunShot;
+    public AudioSource WeaponReload;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,52 +37,67 @@ public class WeaponController : MonoBehaviour
         {
             timeFire = Time.time + 1f / fireRate;
             Shoot();
-             }
+        }
         if (Input.GetButton("Reload") && ammo < ammoStock)
         {
             if (ammoMag != 0)
             {
                 trigger = false;
+                WeaponReload.Play();
                 StartCoroutine(waitReload());
-                }
+            }
             else
             {
                 Debug.Log("Peluru Habis");
-                }
             }
+        }
+        if (AIEnemyController.GiveDamage == true)
+        {
+            health -= AIEnemyController.enemyDamage;
+            healthBar.value = health;
+            if (health <= 0)
+            {
+                Debug.Log("Player Mati");
+                SceneManager.LoadScene("Game Over");
+                AIEnemyController.GiveDamage = false;
+                Restart();
+            }
+        }
     }
 
     IEnumerator waitReload()
-    {
-        yield return new WaitForSeconds(2.5f);
-        if (ammoMag <= ammoStock)
         {
-            ammo = (ammo + ammoMag);
-            add = ammo - ammoStock;
-            if (add < 0)
+            yield return new WaitForSeconds(2.5f);
+            if (ammoMag <= ammoStock)
             {
-                ammoMag = 0;
-                 }
+                ammo = (ammo + ammoMag);
+                add = ammo - ammoStock;
+                if (add < 0)
+                {
+                    ammoMag = 0;
+                }
+                else
+                {
+                    ammoMag = add;
+                    ammo = ammoStock;
+                    Debug.Log("Isi Peluru");
+                }
+            }
             else
             {
-                ammoMag = add;
-                ammo = ammoStock;
-                Debug.Log("Isi Peluru");
-                 }
+                add = (ammo - ammoStock) * -1;
+                ammo = ammo + add;
+                ammoMag = ammoMag - add;
             }
-         else
-        {
-            add = (ammo - ammoStock) * -1;
-            ammo = ammo + add;
-            ammoMag = ammoMag - add;
-             }
-        trigger = true;
-    }
+            trigger = true;
+        }
 
     public void Shoot()
     {
         if (ammo != 0)
         {
+            AK47Shot.Play();
+            ShotgunShot.Play();
             RaycastHit hit;
             Ray ray = new Ray(transform.position, transform.forward);
             if (Physics.Raycast(ray, out hit, range))
@@ -100,4 +119,5 @@ public class WeaponController : MonoBehaviour
         ammoMag = 90;
         ammo = ammoStock;
     }
+    
 }
